1c8e6cf88f4831d35b83defa7b578bfe1dd9f5da
ryoji.ban <r3b3a0n@gmail.com>
2019-10-14 01:42:58 +0900

    定期的にSlackにポストするように

--- RemainingPRNotifyer/index.js
+++ RemainingPRNotifyer/index.js
@@ -2,18 +2,37 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
+
+const createScheduler = require('probot-scheduler')
+const { WebClient } = require('@slack/web-api');
+const slackAPI = new WebClient(process.env.SLACK_BOT_TOKEN);
+
 module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  createScheduler(app, {
+    delay: !!process.env.DISABLE_DELAY, // delay is enabled on first run
+    // interval: 24 * 60 * 60 * 1000 // 1 day
+    interval: 1 * 1 * 60 * 1000 // 1 min
   })
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on('schedule.repository', context => {
+    const { owner, repo } = context.repo()
+    const repoText = `Repository: ${owner}/${repo}`
+    postSlack(repoText)
+  })
+}
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+async function postSlack(text) {
+  console.log(text)
+  try {
+    const result = await slackAPI.chat.postMessage({
+      text: `test`,
+      channel: `${process.env.SLACK_CHANNEL}`,
+    });
+    console.log('Message sent successfully', result.ts);
+  } catch (error) {
+    console.log('An error occurred', error);
+  }
 }
