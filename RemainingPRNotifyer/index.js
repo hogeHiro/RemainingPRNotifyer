feeec44f673d1fe81ce802aa5d7d4d3ffbc06184
ryoji.ban <r3b3a0n@gmail.com>
2019-10-14 02:28:13 +0900

    すべてのプルリクエストを取得してSlackに通知する

--- RemainingPRNotifyer/index.js
+++ RemainingPRNotifyer/index.js
@@ -18,21 +18,43 @@ module.exports = app => {
   })
 
   app.on('schedule.repository', context => {
-    const { owner, repo } = context.repo()
-    const repoText = `Repository: ${owner}/${repo}`
-    postSlack(repoText)
+    notifyPullRequestToSlack(context)
   })
+}
+
+async function notifyPullRequestToSlack(context) {
+    const { owner, repo } = context.repo()
+    const prList = await getAllPullRequests(context, owner, repo)
+    const prText = await getPullRequestsText(prList)
+    postSlack(prText)
 }
 
 async function postSlack(text) {
   console.log(text)
   try {
     const result = await slackAPI.chat.postMessage({
-      text: `test`,
+      text: `${text}`,
       channel: `${process.env.SLACK_CHANNEL}`,
     });
     console.log('Message sent successfully', result.ts);
   } catch (error) {
     console.log('An error occurred', error);
   }
+}
+
+async function getPullRequestsText(prList) {
+  return prList.map(x => `${x.number}: ${x.title} by ${x.user.login}`).join(`¥n`)
+}
+
+async function getAllPullRequests(context, owner, repo) {
+  let pullRequests = await context.github.paginate(
+    context.github.pullRequests.getAll({
+      owner,
+      repo,
+      state: 'all',
+      per_page: 100
+    }),
+    res => res.data
+  )
+  return pullRequests
 }
